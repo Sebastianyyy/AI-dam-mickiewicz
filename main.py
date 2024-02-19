44e74d9fc25e31dd2da4b4645cf039309f1b7544
Sebastian Musial (Sebastianyyy) <64799476+Sebastianyyy@users.noreply.github.com>
2024-02-19 23:54:41 +0100

    loop basic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse 
 import data
 from torch.utils.data import DataLoader
+from utils import train_one_epoch, evaluate
+import torch
+
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--train",action="store_true")
@@ -16,6 +19,8 @@ parser.add_argument("--embedding-dim", action="store", default="36")
 parser.add_argument("--seq-len", action="store", default="8")
 parser.add_argument("--dropout",action="store",default="0.2")
 parser.add_argument("--num-blocks",action="store",default=4)
+parser.add_argument("--epoch",action="store",default=10)
+
 
 args = parser.parse_args()
 
@@ -26,8 +31,21 @@ train, val, test = data_obj.build_datasets()
 
 val=data.MickiewiczDataSet(val,4)
 
-train_dataloader = DataLoader(val, batch_size=4)
+train_dataloader = DataLoader(train, batch_size=args.batch_size)
 
+val_dataloader = DataLoader(val, batch_size=args.batch_size)
 
+test_dataloader = DataLoader(test, batch_size=args.batch_size)
+model=None
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-
+for epoch in range(args.epoch):
+    model.train(True)
+    
+    train_one_epoch(model=model,criterion=criterion,train_loader=train_dataloader,optimizer=optimizer,device=args.device,epoch=epoch)
+    
+    model.eval()
+    
+    evaluate(model=model,data_loader=val_dataloader,device=args.device)
+    
